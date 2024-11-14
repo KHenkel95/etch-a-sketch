d39c179894cf30b9904ae823efe5ee5fabcfb6f5
Ken Henkel <kenhenkel95@gmail.com>
2024-11-14 12:08:28 -0500

    Properly size the squares relative to grid size
    Properly sized the squares into the grid in javascript.js by using
    template literals with DOM manipulation. Also updated the README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # etch-a-sketch
+
+This project is a part of The Odin Project
+
+(Project Directions)[https://www.theodinproject.com/lessons/foundations-etch-a-sketch]
--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
 const container = document.querySelector(".container");
 
-for(let i = 0; i < 256; i++){
+let containerSize = 600;
+let gridSize = 16;
+let squareSize = containerSize/gridSize;
+
+for(let i = 0; i < (16*16); i++){
     const square = document.createElement("div");
     square.classList.add("squares");
+    square.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px`);
     container.appendChild(square);
 }
