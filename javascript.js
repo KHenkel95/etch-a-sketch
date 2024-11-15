546c32ac7f53ea1c86a4685671b32762298cbc17
Ken Henkel <kenhenkel95@gmail.com>
2024-11-15 12:20:27 -0500

    Create colorSquare function & refactor createGrid
    Created a colorSquare function to color the squares of the grid and
    refactored the rest of the code that created the grid into its own
    createGrid function to avoid global variables

--- javascript.js
+++ javascript.js
@@ -1,12 +1,24 @@
-const container = document.querySelector(".container");
-
-let containerSize = 600;
-let gridSize = 16;
-let squareSize = containerSize/gridSize;
-
-for(let i = 0; i < (16*16); i++){
-    const square = document.createElement("div");
-    square.classList.add("squares");
-    square.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px`);
-    container.appendChild(square);
+function createGrid(){
+    const container = document.querySelector(".container");
+    let containerSize = 600;
+    let gridSize = 50;
+    let squareSize = containerSize/gridSize;
+    for(let i = 0; i < (gridSize**2); i++){
+        const square = document.createElement("div");
+        square.classList.add("squares");
+        square.setAttribute("style", `width: ${squareSize}px; height: ${squareSize}px`);
+        container.appendChild(square);
+    }
 }
+
+function colorSquares(){
+    const squares = document.querySelectorAll(".squares");
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "black";
+        });
+    });
+}
+
+createGrid();
+colorSquares();
